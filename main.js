6e7ebbf46382eb212d06d2e3d0f9a9eca71056c3
20taxitaxi00 <takushi0210@icloud.com>
2020-12-08 20:30:58 +0900

    ドロワー途中

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 $(function(){
 
-    // // ハンバーガーメニュー
-	// $('#btn').on('click',function(){
-	// 	let isActive = $(this).hasClass('on');
-	// 	toggleDrawer(isActive);
-	// });
+    // ハンバーガーメニュー
+	$('#btn').on('click',function(){
+		let isActive = $(this).hasClass('on');
+		toggleDrawer(isActive);
+	});
 
-	// // ドロワー
-	// function toggleDrawer(isActive) {
-	//     $('.js-hamburger')
-	//     if (isActive) {
-	//       $('#drawer-bg').fadeOut(600);
-	//     } else {
-	//       $('#drawer-bg').fadeIn(600);
-	//     }
-	//     $('.js-hamburger').toggleClass('on');
-	//     $('.js-drawer').toggleClass('on');
-	//   }
+	// ドロワー
+	function toggleDrawer(isActive) {
+	    $('.js-hamburger')
+	    if (isActive) {
+	      $('#drawer-bg').fadeOut(600);
+	    } else {
+	      $('#drawer-bg').fadeIn(600);
+	    }
+	    $('.js-hamburger').toggleClass('on');
+	    $('.js-drawer').toggleClass('on');
+	  }
 
 
 
