a615acd3502eb9c78b78bc1ff688901568e46925
20taxitaxi00 <takushi0210@icloud.com>
2020-12-08 15:57:07 +0900

    ハンバーガー調整とjs追加（コメントアウトなう）

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 $(function(){
 
+    // // ハンバーガーメニュー
+	// $('#btn').on('click',function(){
+	// 	let isActive = $(this).hasClass('on');
+	// 	toggleDrower(isActive);
+	// });
 
+	// // ドロワー
+	// function toggleDrower(isActive) {
+	//     $('.js-hamburger')
+	//     if (isActive) {
+	//       $('#drawer-bg').fadeOut(600);
+	//     } else {
+	//       $('#drawer-bg').fadeIn(600);
+	//     }
+	//     $('.js-hamburger').toggleClass('on');
+	//     $('.js-drawer').toggleClass('on');
+	//   }
 
 
 
