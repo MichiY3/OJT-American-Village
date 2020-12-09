aa3f3bf8ef1a5d2e175d249d8ff1f27cbf63d154
20taxitaxi00 <takushi0210@icloud.com>
2020-12-09 16:25:33 +0900

    スライダー途中

--- main.js
+++ main.js
@@ -18,10 +18,23 @@ $(function(){
 	    $('.js-drawer').toggleClass('on');
 	  }
 
-
-
-
-
+	// スライダー
+	// var $setElm = $('.slider'),
+    // fadeSpeed = 1500,
+    // switchDelay = 3000;
+ 
+    // $setElm.each(function(){
+    //     var targetObj = $(this);
+	// 	var findImg = targetObj.find('picture');
+    //     var findLiFirst = targetObj.find('picture:first');
+ 
+	// 	findImg.css({display:'block',opacity:'0',zindex:'99'});
+	// 	findLiFirst.css({zIndex:'100'}).stop().animate({opacity:'1'},fadeSpeed);
+ 
+    //     setInterval(function(){
+    //         targetObj.find('picture:first-child').animate({opacity:'0'},fadeSpeed).next('li').css({zIndex:'100'}).animate({opacity:'1'},fadeSpeed).end().appendTo(targetObj).css({zIndex:'99'});
+    //     },switchDelay);
+    // });
 
     
 });
